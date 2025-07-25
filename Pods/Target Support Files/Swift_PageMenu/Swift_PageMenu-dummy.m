4affe11eaa196e74b54057387999252ec8bb1336
Ramanpreet123 <Ramanpreet2120@gmail.com>
2025-07-25 17:48:04 +0530

    removed files
    removed files

--- Pods/Target Support Files/Swift_PageMenu/Swift_PageMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Swift_PageMenu : NSObject
-@end
-@implementation PodsDummy_Swift_PageMenu
-@end
--- SearchPoint/Model/DeleteGroupModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DeleteGroupModel.swift
-//  SearchPoint
-//
-//  Created by Mobile Programming on 18/05/21.
-//
-
-import Foundation
