8cb9bb2e0d460e55ddd94d13ec1d40f1ac290437
Ramanpreet123 <Ramanpreet2120@gmail.com>
2025-08-25 16:38:52 +0530

    files removed
    files removed

--- SearchPoint/ResolveIssuesVC/ResolveIssuesCollectionViewCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ResolveIssuesCollectionViewCell.swift
-//  SearchPoint
-//
-//  Created by Ramanpreet Singh on 22/04/25.
-//
-
-import Foundation
--- SearchPoint/ResolveIssuesVC/ResolveIssuesiPadVC.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ResolveIssuesiPadVC.swift
-//  SearchPoint
-//
-//  Created by Ramanpreet Singh on 21/04/25.
-//
-
-import Foundation
