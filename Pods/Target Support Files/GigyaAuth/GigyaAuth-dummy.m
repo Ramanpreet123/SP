6459b22a3bbaab9c6dc13d3423f23bc9993cc37d
Ramanpreet123 <Ramanpreet2120@gmail.com>
2025-08-25 16:54:16 +0530

    files added
    files added

--- Pods/Target Support Files/GigyaAuth/GigyaAuth-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GigyaAuth : NSObject
-@end
-@implementation PodsDummy_GigyaAuth
-@end
--- Pods/Target Support Files/GigyaTfa/GigyaTfa-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GigyaTfa : NSObject
-@end
-@implementation PodsDummy_GigyaTfa
-@end
